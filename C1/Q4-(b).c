9608c444807771865b0afb0c4c58e313deb15da8
Melvin N <melvin.2025.n@gmail.com>
2025-12-05 20:16:13 +0530

    Update Q4-(b).c

--- C1/Q4-(b).c
+++ C1/Q4-(b).c
@@ -1,2 +1,23 @@
 //Write a C program to create an array of pointers and perform operations on it
 
+#include<stdio.h>
+
+int main() {
+    
+    char *p[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+    
+    int dayno;
+    
+    printf("Enter day number (1-7): ");
+    scanf("%d",&dayno);
+    
+    printf("Day corresponding to day number %d is %s",dayno,p[dayno-1]);
+    
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter day number (1-7): 5
+Day corresponding to day number 5 is Friday
+*/
