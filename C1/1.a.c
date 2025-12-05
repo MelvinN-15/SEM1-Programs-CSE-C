dbd5cfe0982c6e46f65f9df61612f0b203ef5703
Melvin N <melvin.2025.n@gmail.com>
2025-12-05 17:22:15 +0530

    Create 1.a.c

---
+++ C1/1.a.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    
+    int radius;
+    float area,circumference;
+    
+    printf("Enter radius: ");
+    scanf("%d",&radius);
+    
+    area = 3.14*radius*radius;
+    circumference = 2*3.14*radius;
+    
+    printf("\nArea of the circle: %.2f",area);
+    printf("\nCircumference of the circle: %.2f",circumference);
+    
+    return 0;
+}
