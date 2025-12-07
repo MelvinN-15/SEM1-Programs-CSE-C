ea86f44d8944c610d4689dc851de66a522092f94
Melvin N <melvin.2025.n@gmail.com>
2025-12-07 20:45:21 +0530

    Update Q-17-(a).c

--- C1/Q-17-(a).c
+++ C1/Q-17-(a).c
@@ -1,2 +1,25 @@
 // Write a C program that prints the factorial of a given number
 
+#include <stdio.h>
+
+int main() {
+    
+    int fact=1;
+    int n;
+    
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    
+    for(int i=1; i<=n; i++) {
+        fact*=i;
+    }
+    
+    printf("Factorial of %d is %d",n,fact);
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter a number: 4
+Factorial of 4 is 24
+*/
