7c3d3fd94bed58ac183c2264a2fb56cd52d337f6
Melvin N <melvin.2025.n@gmail.com>
2025-12-07 11:58:46 +0530

    Update Q9-(a).c

--- C1/Q9-(a).c
+++ C1/Q9-(a).c
@@ -1,16 +1,24 @@
+// Write a C program that asks the user for a number and prints whether it's even or odd
+
 #include <stdio.h>
 int main() {
 
-int x;
-printf("Enter a number: ");
-scanf("%d",&x);
+	int x;
+	printf("Enter a number: ");
+	scanf("%d",&x);
 
-if(x%2==0) {
-printf("%d is an even number",x);
-}
-else{
-printf("%d is an odd number",x);
+	if(x%2==0) {
+		printf("%d is an even number",x);
+	}
+	else {
+		printf("%d is an odd number",x);
+	}
+
+	return 0;
 }
 
-return 0;
-}
+/*
+OUTPUT:
+Enter a number: 12
+12 is an even number
+*/
